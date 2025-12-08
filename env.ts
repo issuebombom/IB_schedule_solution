c55fb78d3e156b931fdc06a8c26da0ada2db15bb
issuebombom <issuebombom@gmail.com>
2025-12-08 09:52:18 +0900

    feat: env validator

---
+++ env.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+// 1. 환경 변수 스키마 정의 (z.object)
+const envSchema = z.object({
+  WINGS_MAIN_URL: z.string().min(1),
+  WINGS_COMPANY_ID: z.string().min(1),
+  WINGS_USER_ID: z.string().min(1),
+  WINGS_USER_PW: z.string().min(1),
+  WINGS_SCHEDULE_URL: z.string().min(1),
+  WINGS_SCHEDULE_DETAILS_URL: z.string().min(1),
+});
+
+// 2. process.env 검증 및 결과 객체 생성
+// 환경 변수가 스키마를 만족하지 못하면 에러 발생
+export const ENV = (() => {
+  try {
+    return envSchema.parse(process.env);
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      console.error(err.message);
+    }
+    throw err;
+  }
+})();
