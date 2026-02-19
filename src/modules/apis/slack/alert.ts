a377d5974d4e0c2a29b6c992307872fc00ae5b66
issuebombom <issuebombom@gmail.com>
2026-02-19 14:55:19 +0900

    feat: slack msg for changedEvent

--- src/modules/apis/slack/alert.ts
+++ src/modules/apis/slack/alert.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { format } from 'date-fns';
-import { ParsedWingsSchedules, WingsSchedulesValues } from '../../types/schedules.type';
-import { ParsedGoogleCalendar } from '../../types/calendar.type';
+import { DiffEventFieldValue } from '../../types/schedules.type';
+import { ParsedBatchResponse, ParsedGoogleCalendar } from '../../types/calendar.type';
 
 export class SlackAlert {
   private token;
@@ -22,39 +22,93 @@ export class SlackAlert {
     await axios.post(url, messageForm, { headers });
   }
 
-  newEventMessage(
-    newEventNumbers: Set<string>,
-    currSchedules: ParsedWingsSchedules,
-    newCalendars: ParsedGoogleCalendar,
-  ) {
-    const events: WingsSchedulesValues[] = [];
+  newEventMessage(newEventNumbers: Set<string>, newCalendars: ParsedGoogleCalendar) {
+    const events: ParsedBatchResponse[] = [];
     newEventNumbers.forEach((eNumber: string) => {
-      const schedule = currSchedules.get(eNumber);
-      // 취소 설정된 신규 일정은 캘린더 등록 안함
-      if (schedule && schedule.status !== 'CXL') {
+      const schedule = newCalendars.get(eNumber);
+      if (schedule) {
         events.push(schedule);
       }
     });
 
     const subject = `:pushpin:  총 ${events.length}건의 새로운 행사가 등록되었습니다.\n`;
-    const message = events.reduce((prevMsg, event: WingsSchedulesValues) => {
+    const message = events.reduce((prevMsg, event: ParsedBatchResponse) => {
+      const property = event.extendedProperties.shared;
       let msg = '';
-      const startTime = format(new Date(event.startTime), 'MM/dd EEE HH:mm');
-      const endTime = format(new Date(event.endTime), 'MM/dd EEE HH:mm');
-      const details = this.replaceArrayToString(event.details);
-      const link = newCalendars.get(event.eventNumber)?.htmlLink;
+      const startTime = format(new Date(property.startTime), 'MM/dd EEE HH:mm');
+      const endTime = format(new Date(property.endTime), 'MM/dd EEE HH:mm');
+      const detail = property.detail || '없음';
+      const link = event.htmlLink;
 
-      msg += `_*${event.eventName}*_ [${startTime}]\n`;
-      msg += `> 장소: ${event.place}\n`;
+      msg += `_*${property.eventName}*_ [${startTime}]\n`;
+      msg += `> 장소: ${property.place}\n`;
       msg += `> 행사날짜:\n`;
       msg += `> ${startTime} ~ ${endTime}\n`;
-      msg += `> 담당자: ${event.manager}\n`;
+      msg += `> 담당자: ${property.manager}\n`;
       msg += `> 참조사항:\n`;
-      msg += `> ${this.shorten(details)}\n`;
-      msg += `> <${link}|자세히>`;
+      msg += `> ${this.shorten(detail)}\n`;
+      msg += `> <${link}|자세히>\n`;
 
       return prevMsg + msg;
     }, '');
+
+    return { subject, message };
+  }
+
+  changedEventMessage(events: DiffEventFieldValue[]) {
+    const nameMatch = {
+      eventNumber: '행사번호',
+      eventName: ':bookmark:  행사명',
+      manager: ':man_in_tuxedo:  매니저',
+      startTime: ':alarm_clock:  시작 날짜',
+      endTime: ':alarm_clock:  종료 날짜',
+      place: ':house:  장소',
+      status: ':white_check_mark:  예약 상태',
+      details: ':notebook:  참조 사항',
+    };
+
+    /**
+     * 하나의 행사 내에서도 복수의 필드값이 변경되었을 수 있다.
+     * 이 경우는 하나의 행사가 내용 변경된 것으로 취급해야 한다.
+     * */
+    const changedCount = new Set(events.map((e) => e.eventNumber)).size; // 중복 행사 제외
+    const subject = `:dizzy:  총 ${changedCount}건의 행사 정보가 변경되었습니다.\n`;
+
+    let recentEventNumber = '';
+    const message = events.reduce((prevMsg, event: DiffEventFieldValue) => {
+      let msg = '';
+
+      // 전처리
+      const startTime = format(new Date(event.startTime), 'MM/dd EEE HH:mm');
+      const changedFieldName = nameMatch[event.eventField as keyof typeof nameMatch];
+      const prevValue = Array.isArray(event.prevValue)
+        ? this.replaceArrayToString(event.prevValue)
+        : event.prevValue;
+      const currValue = Array.isArray(event.currValue)
+        ? this.replaceArrayToString(event.currValue)
+        : event.currValue;
+
+      // 동일 행사에 여러 변경 항목이 있을 경우 1타이틀 + n 변경사항으로 알림 내용 작성
+      const title =
+        recentEventNumber !== event.eventNumber
+          ? `_*${event.eventName}*_ [${startTime}] \`${event.place}\`\n`
+          : '';
+
+      msg += `> ${changedFieldName}\n`;
+      msg += `> \`변경 전\`  ${prevValue}\n`;
+      msg += `> \`변경 후\`  ${currValue}\n`;
+
+      recentEventNumber = event.eventNumber;
+
+      return prevMsg + title + msg;
+    }, '');
+
+    return { subject, message };
+  }
+
+  errorMessage(step: string, errMessage: string, stack?: string) {
+    const subject = `${step}가 발생했습니다.`;
+    const message = `${errMessage}\ntraceback:\n${stack}`;
 
     return { subject, message };
   }
@@ -80,10 +134,11 @@ export class SlackAlert {
     };
   }
 
+  // 참조 사항은 배열 타입이므로 문자열로 변경한다.
   private replaceArrayToString(arr: string[]) {
-    let contents = '없음';
+    let contents = '참조사항 없음';
     if (arr.length > 0) {
-      contents = arr.map((v) => v.replace(/\n/g, ' ')).join('');
+      contents = arr.map((v) => v.replace(/\n/g, ' ')).join(' ');
     }
     return contents;
   }
